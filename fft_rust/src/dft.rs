134faa0fe5c2e60d4997edddc22584fff960b008
aki <aki@mymail.com>
2024-03-22 18:25:31 +0900

    フーリエ変換・逆フーリエ変換をフラッグで切り替えられるようにした

--- fft_rust/src/dft.rs
+++ fft_rust/src/dft.rs
@@ -19,7 +19,40 @@ pub fn dft(array: &Vec<f64>) -> Vec<Complex64> {
     result
 }
 
+// is_reverseがtrueのときは逆フーリエ変換を実行する
+pub fn dft_2(array: &Vec<Complex64>, is_reverse: bool) -> Vec<Complex64> {
+    let n = array.len();
+    let mut result = vec![Complex64::new(0.0, 0.0); n];
+
+    for l in 0..n {
+        for (m, v) in array.iter().enumerate() {
+            let mut rot_factor = Complex64::new(
+                0.0,
+                -2.0 * std::f64::consts::PI * (l as f64 * m as f64) / n as f64,
+            )
+            .exp();
+
+            if is_reverse {
+                rot_factor = rot_factor.conj() / Complex64::new(n as f64, 0.0);
+            }
+
+            result[l] += rot_factor * v;
+        }
+    }
+
+    result
+}
+
 fn main() {
     let array = vec![1.2, 1.3, 1.4, 1.5, 1.6, 1.7];
     println!("array_ft: {:?}", dft(&array));
+
+    // フーリエ変換したものを逆フーリエ変換して確かめる
+    let array = vec![1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
+        .iter()
+        .map(|x| Complex64::new(*x, 0.0))
+        .collect();
+    let res_dft = dft_2(&array, false);
+    let res_dft_rev: Vec<f64> = dft_2(&res_dft, true).iter().map(|x| x.re).collect();
+    println!("res_dft_rev: {:?}", res_dft_rev);
 }
